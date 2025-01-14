5f853eb363248f2a7d710fba2f4b5bc1efffd008
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-01-14 22:33:02 +0530

    some bug fixes

--- src/app.js
+++ src/app.js
@@ -12,33 +12,27 @@ app.use(
 );
 
 // for normal Json
-app.use(express.json({
-  limit: "16kb",
-
-}));
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 // for url request
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
-}))
+  })
+);
 // this is for public assets
 app.use(express.static("public"));
 // for cookies // for secure cookie CRED Options
 app.use(cookieParser());
-
 
 //routes
 import userRoutes from "./routes/user.routes.js";
 
 //routes Declaration
 app.use("/api/v1/users", userRoutes);
-
-
-
-
-
-
-
 
 export { app };
